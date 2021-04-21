b28b2ef3fa5c23ec69f68df1f812418a3f8c87bb
Jakub Haliniak <jakubh.fake@gmail.com>
2021-04-21 22:39:11 +0200

    Add Accordion only click on first product working

--- src/js/script.js
+++ src/js/script.js
@@ -88,10 +88,10 @@
         console.log('xxx: ',activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if(activeProduct !== null && activeProduct !== thisProduct.element) {
-          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+          activeProduct.classList.remove('active');
         }
         /* toggle active class on thisProduct.element */
-        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        thisProduct.element.classList.toggle('active');
       });
     }
   }
