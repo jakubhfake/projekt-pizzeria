58428bdacde9bc57c409b899a85dea468f27deff
Jakub Haliniak <jakubh.fake@gmail.com>
2021-04-21 19:29:16 +0200

    Add Accordion in products

--- src/js/script.js
+++ src/js/script.js
@@ -58,19 +58,40 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
     renderInMenu(){
       const thisProduct = this;
       /* generate HTML based of templates */
       const generatedHTML = templates.menuProduct(thisProduct.data);
-      console.log('Product HTML: ', generatedHTML);
+      // console.log('Product HTML: ', generatedHTML);
       /* create element using utils.createElementFromHTML */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       /* find menu container */
       const menuContainer = document.querySelector(select.containerOf.menu);
       /*add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+            
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelectorAll(classNames.menuProduct.wrapperActive);
+        console.log('xxx: ',activeProduct);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct !== thisProduct.element) {
+          thisProduct.classList.remove('active');}
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
@@ -97,6 +118,6 @@
       thisApp.initData();
       thisApp.initMenu();
     },
-  }
+  };
   app.init();
 }
