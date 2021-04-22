8f31560b25300421ba4a5c561ad7448cecc44cb3
Jakub Haliniak <jakubh.fake@gmail.com>
2021-04-22 21:05:15 +0200

    Deleted project descriptions

--- src/js/script.js
+++ src/js/script.js
@@ -63,14 +63,9 @@
     }
     renderInMenu(){
       const thisProduct = this;
-      /* generate HTML based of templates */
       const generatedHTML = templates.menuProduct(thisProduct.data);
-      // console.log('Product HTML: ', generatedHTML);
-      /* create element using utils.createElementFromHTML */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-      /* find menu container */
       const menuContainer = document.querySelector(select.containerOf.menu);
-      /*add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
 
@@ -87,20 +82,13 @@
     initAccordion(){
       const thisProduct = this;
   
-      /* find the clickable trigger (the element that should react to clicking) */
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-            
-      /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
-        /* prevent default action for event */
-        event.preventDefault();
-        /* find active product (product that has active class) */
+        event.preventDefault();        
         const activeProduct = document.querySelector(select.all.menuProductsActive);
-        /* if there is active product and it's not thisProduct.element, remove class active from it */
         if(activeProduct !== null && activeProduct !== thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
-        /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
