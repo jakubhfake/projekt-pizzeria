377fd35b09bf429173ec05855fd1750a9a124657
Jakub Haliniak <jakubh.fake@gmail.com>
2021-05-31 15:35:30 +0200

    Division into components

---
+++ src/js/components/AmountWidget.js
@@ -0,0 +1,68 @@
+import {settings, select} from './settings.js';
+
+class AmountWidget {
+  constructor(element) {
+    const thisWidget = this;
+    thisWidget.value = settings.amountWidget.defaultValue;
+    thisWidget.getElements(element);
+    thisWidget.setValue(thisWidget.input.value);
+    thisWidget.initActions();
+      
+  }
+  getElements(element) {
+    const thisWidget = this;
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(
+      select.widgets.amount.input
+    );
+    thisWidget.linkDecrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkDecrease
+    );
+    thisWidget.linkIncrease = thisWidget.element.querySelector(
+      select.widgets.amount.linkIncrease
+    );
+    thisWidget.minProductValue = settings.amountWidget.defaultMin;
+    thisWidget.maxProductValue = settings.amountWidget.defaultMax;
+  }
+
+  announce() {
+    const thisWidget = this;
+    const event = new Event('updated', {
+      bubbles: true
+    });
+    thisWidget.element.dispatchEvent(event);
+  }
+
+  setValue(value) {
+    const thisWidget = this;
+    const newValue = parseInt(value);
+    if (
+      thisWidget.value !== newValue &&
+        !isNaN(newValue) &&
+        newValue >= thisWidget.minProductValue &&
+        newValue <= thisWidget.maxProductValue
+    ){
+      thisWidget.value = newValue;
+    }
+    thisWidget.input.value = thisWidget.value;
+    thisWidget.announce();
+  }
+  initActions() {
+    const thisWidget = this;
+
+    thisWidget.input.addEventListener('change', function () {
+      thisWidget.setValue(thisWidget.input.value);
+    });
+
+    thisWidget.linkDecrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+
+    thisWidget.linkIncrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+  }
+}
+export default AmountWidget;
---
+++ src/js/components/Cart.js
@@ -0,0 +1,129 @@
+import {settings, select, classNames, templates} from './settings.js';
+import utils from '../utils';
+
+import CartProduct from './components/CartProduct.js';
+
+class Cart {
+  constructor(element) {
+    const thisCart = this;
+    thisCart.products = [];
+    thisCart.getElements(element);
+    thisCart.initActions();
+    console.log('thisCart.products', thisCart.products); 
+  }
+
+  initActions() {
+    const thisCart = this;
+    thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+    });
+    thisCart.dom.productList.addEventListener('updated', function(){
+      thisCart.update();
+    });
+    thisCart.dom.productList.addEventListener('remove', function(event) {
+      thisCart.remove(event.detail.cartProduct);
+    });
+    thisCart.dom.form.addEventListener('submit', function(event){
+      event.preventDefault();
+      thisCart.sendOrder();
+    });
+  }
+
+  getElements(element) {
+    const thisCart = this;
+    thisCart.dom = {};
+    thisCart.dom.wrapper = element;
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
+    thisCart.dom.deliveryFee = thisCart.dom.wrapper.querySelector(select.cart.deliveryFee);
+    thisCart.dom.subTotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
+    thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
+    thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+    thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+    thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
+    thisCart.dom.address = thisCart.dom.wrapper.querySelector(select.cart.address);
+
+  }
+
+  add(menuProduct) {
+    const thisCart = this;
+    const generatedHTML = templates.cartProduct(menuProduct);
+    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+    thisCart.dom.productList.appendChild(generatedDOM);
+    thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+    thisCart.update();
+  }
+
+  update() {
+    const thisCart = this;
+    let deliveryFee = settings.cart.defaultDeliveryFee;
+    thisCart.totalNumber = 0;
+    thisCart.subTotalPrice = 0;
+    
+    for (let product of thisCart.products) {
+      thisCart.totalNumber += product.amount;
+      thisCart.subTotalPrice += product.price;
+    }
+
+    if (thisCart.totalNumber !== 0 ) {
+      thisCart.totalPrice = thisCart.subTotalPrice + deliveryFee;
+      thisCart.dom.deliveryFee.innerHTML = deliveryFee;
+      thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
+      thisCart.dom.subTotalPrice.innerHTML = thisCart.subTotalPrice;
+      for(let cartTotalPrice of thisCart.dom.totalPrice){
+        cartTotalPrice.innerHTML = thisCart.totalPrice;
+      }
+        
+    }
+    else {
+      thisCart.totalPrice = 0;
+      deliveryFee = 0;
+    }
+  }
+  remove(cartProduct) {
+    const thisCart = this;
+
+    cartProduct.dom.wrapper.remove();
+    const indexOfProduct = thisCart.products.indexOf(cartProduct);
+    const productRemovedValues = thisCart.products.splice(indexOfProduct, 1);
+    console.log('Removed product from array', productRemovedValues);
+    thisCart.update();
+  }
+  sendOrder() {
+    const thisCart = this;
+    const url = settings.db.url + '/' + settings.db.orders;
+    const payload = {
+      address: thisCart.dom.address.value,
+      phone: thisCart.dom.phone.value,
+      totalPrice: thisCart.totalPrice,
+      subTotalPrice: thisCart.subTotalPrice,
+      totalNumber: thisCart.totalNumber,
+      deliveryFee: thisCart.deliveryFee,
+      products: [],
+    };
+    for(let prod of thisCart.products) {
+      payload.products.push(prod.getData());
+    }
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+      
+    fetch(url, options)
+      // .then nie jest potrzebne
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+      });
+
+    console.log('Order', payload);
+    console.log('url', url);
+  }
+}
+export default Cart;
