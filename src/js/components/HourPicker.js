1728530c16d77a43c880df6b6d1dcafabfe87b6e
Jakub Haliniak <jakubh.fake@gmail.com>
2021-06-10 11:35:39 +0200

    Working on class HourPicker

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,13 +1,19 @@
-//import {settings, select} from '../settings.js';
+import {settings, select} from '../settings.js';
 //import utils from '../utils.js';
-//import BaseWidget from './BaseWidget.js';
-
 import BaseWidget from './BaseWidget.js';
 
 class HourPicker extends BaseWidget {
   constructor(wrapper) {
+    super (wrapper, settings.hours.open);
     const thisWidget = this;
 
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widget.hourPicker.output);
+
+  }
+  initPlugin() {
+    const thisWidget = this;
+    rangeSlider.create(thisWidget.dom.input);
   }
 
   parseValue(value){
