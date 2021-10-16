6e9751715d39a89535a6275938c1abd4281b3eab
Jakub Haliniak <jakubh.fake@gamil.com>
2021-10-16 12:06:01 +0200

    Working on home page html and scss

---
+++ .history/src/js/components/Home_20211014141721.js
@@ -0,0 +1,34 @@
+import {templates} from '../settings.js';
+
+
+class Home {
+  constructor(element){
+    const thisHome = this;
+    thisHome.render(element);
+    thisHome.initPlugin();
+  }
+
+  render(element) {
+    const thisHome = this;
+    thisHome.dom = {};
+    thisHome.dom.wrapper = element;
+    const generatedHTML = templates.homeWidget();
+    thisHome.dom.wrapper.innerHTML = generatedHTML;
+  }
+
+  initPlugin() {
+    const thisHome = this;
+    const carouselWidget = document.querySelector('.main-carousel');
+    // eslint-disable-next-line no-undef
+    thisHome.flkty = new Flickity( carouselWidget, {
+    // options
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: true,
+      prevNextButtons: false,
+      
+    });
+  }
+
+}
+export default Home;
